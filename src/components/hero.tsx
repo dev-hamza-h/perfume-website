2ff7804947d4a1b43ca957e968855ee23b2fbaa0
Hamza Hassan <hk5732065@gmail.com>
2024-11-13 21:12:50 +0500

    Initial commit

--- src/components/hero.tsx
+++ src/components/hero.tsx
@@ -1,9 +1,10 @@
 import React from 'react';
-import heroImg from "@/assets/heroImg.png"
+import heroImg from "@/assets/heroImg.png";
 import Image from 'next/image';
+
 const Hero: React.FC = () => {
   return (
-    <section className="relative bg-black text-white min-h-screen flex items-center">
+    <section className="relative bg-black text-white min-h-screen flex items-center py-10">
       <div className="container mx-auto flex flex-col md:flex-row-reverse items-center px-6 md:px-12 lg:px-24 space-y-6 md:space-y-0">
         
         {/* Right section containing the image */}
@@ -16,19 +17,17 @@ const Hero: React.FC = () => {
         </div>
 
         {/* Left section containing the text and button */}
-        <div className="w-full md:w-1/2 flex flex-col justify-center h-full text-center md:text-left">
-          <div className="space-y-4">
-            <h1 className="text-3xl sm:text-4xl md:text-5xl font-bold leading-tight">
-              Elevate Your Spirit with Victory Scented Fragrances!
-            </h1>
-            <p className="text-lg text-gray-300">
-              Shop now and embrace the sweet smell of victory with Local Face.
-            </p>
-          </div>
+        <div className="w-full md:w-1/2 flex flex-col justify-center h-full text-center md:text-left space-y-4">
+          <h1 className="text-3xl sm:text-4xl md:text-5xl font-bold leading-tight">
+            Elevate Your Spirit with Victory Scented Fragrances!
+          </h1>
+          <p className="text-lg text-gray-300">
+            Shop now and embrace the sweet smell of victory with Local Face.
+          </p>
 
           {/* Button Container */}
           <div className="mt-6 md:mt-14">
-            <button className="bg-orange-600 hover:bg-orange-700 text-white px-8 py-3 rounded-md font-semibold text-lg">
+            <button className="bg-orange-600 hover:bg-orange-700 text-white px-8 py-3 rounded-md font-semibold text-lg mx-auto md:mx-0">
               Shop Now
             </button>
           </div>
